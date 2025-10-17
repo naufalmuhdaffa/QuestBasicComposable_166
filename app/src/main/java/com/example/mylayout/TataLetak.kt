1c876080d946c796932d3aa9ff28307bf2a95aeb
naufalmuhdaffa <naufaldaffa878@gmail.com>
2025-10-17 10:29:26 +0700

    Add Component

--- app/src/main/java/com/example/mylayout/TataLetak.kt
+++ app/src/main/java/com/example/mylayout/TataLetak.kt
@@ -22,5 +22,11 @@ fun TataletakColumn(modifier: Modifier){
 
 @Composable
 fun TataletakRow(modifier: Modifier) {
-
+    Row(modifier = modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceEvenly) {
+        Text(text = "Komponen1")
+        Text(text = "Komponen2")
+        Text(text = "Komponen3")
+        Text(text = "Komponen4")
+    }
 }
